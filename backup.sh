dcb7dccff61bfd7b133b020c007c858a9fc698ee
MadaooQuake <kielkowskimarcin@prokonto.pl>
2016-05-07 06:21:52 +0200

    finish backuper

--- backup.sh
+++ backup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # prepare date
-now=$(date +'%m-%d-%Y')
-echo $now
+now=Website-$(date +'%m-%d-%Y')
 
 # copy website - all website with images and themes
-
-
+cd ~/backups
+mkdir ~/backups/$now
+cp -rv ~/domains/Website.com/* ~/backups/$now
 # copy wp database
+mysqldump -u user -ppassword WebsiteDB > ~/backups/$now/$now.sql
 
 # zip file
+cd ~/backups
+tar -zcvf ~/backups/$now.tar.gz ~/backups/$now
+rm -rv $now
